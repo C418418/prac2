90da2647a043d7f08e589a1c010ada2b84d4d254
C418418 <friday888777@gmail.com>
2024-11-19 14:14:49 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WeatherTranslator;
+
+namespace evdokimovpractice2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 secondWindow = new Window1();
+            secondWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 rdWindow = new Window2();
+            rdWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Window3 frdWindow = new Window3();
+            frdWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window4 pfrdWindow = new Window4();
+            pfrdWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using evdokimovpractice2;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace WeatherTranslator
+{
+    public partial class Window1 : Window
+    {
+        private Dictionary<string, string> translations;
+
+        public Window1()
+        {
+            InitializeComponent();
+            InitializeTranslations();
+        }
+
+        private void InitializeTranslations()
+        {
+            translations = new Dictionary<string, string>
+            {
+                { "солнце", "sun" },
+                { "дождь", "rain" },
+                { "снег", "snow" },
+                { "ветер", "wind" },
+                { "облако", "cloud" },
+                { "туман", "fog" },
+                { "град", "hail" },
+                { "молния", "lightning" },
+                { "гроза", "thunderstorm" },
+                { "температура", "temperature" }
+            };
+        }
+
+        private void TranslateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text.Trim().ToLower();
+            if (translations.TryGetValue(input, out string translation))
+            {
+                TranslationTextBlock.Text = translation;
+            }
+            else
+            {
+                TranslationTextBlock.Text = "Такого слова нет.";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow svo = new MainWindow();
+            svo.Show();
+            Close();
+        }
+    }
+}
